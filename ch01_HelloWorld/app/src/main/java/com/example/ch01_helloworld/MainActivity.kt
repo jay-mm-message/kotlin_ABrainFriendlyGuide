fb5a055355f42053961b81d9ff4e3f3e5d07ec98
jay <jay.mm.message@gmail.com>
2022-04-10 23:59:34 +0800

    Test cast output "YabbaDabbaDo".

--- ch01_HelloWorld/app/src/main/java/com/example/ch01_helloworld/MainActivity.kt
+++ ch01_HelloWorld/app/src/main/java/com/example/ch01_helloworld/MainActivity.kt
@@ -60,5 +60,13 @@ class MainActivity : AppCompatActivity() {
         var y = 1;
 
         println(if (x > y ) "$TAG $x is greater than $y." else "$TAG $x isn't greater than $y.");
+
+        x = 1;
+        while( x < 3 ) {
+            print(if (x == 1) "Yab" else "Dab");
+            print("ba");
+            x += 1;
+        }
+        if (x == 3) println("Do");
     }
 }
